e49be893c576eb08e6a20db6ccf5c3f3737813c4
FeiFei0827 <2035585231@qq.com>
2023-08-26 16:08:58 +0800

    飞飞试一试

--- db/model/user.go
+++ db/model/user.go
@@ -5,6 +5,6 @@ import "gorm.io/gorm"
 // User 用户表 /*
 type User struct {
 	gorm.Model
-	Username string  `gorm:"not null;unique;size: 32;index"`
-	Password *string `gorm:"not null;size: 32"`
+	Username string `gorm:"not null;unique;size: 32;index"`
+	Password string `gorm:"not null;size: 32"`
 }
