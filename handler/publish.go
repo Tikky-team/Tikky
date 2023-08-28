f8473d5687957696326dbff84ab82f9a6bddb7f5
luo xue <2426296869@qq.com>
2023-08-28 19:03:04 +0800

    feed and publish interface

--- handler/publish.go
+++ handler/publish.go
@@ -9,9 +9,11 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/segmentio/ksuid"
 	ffmpeg_go "github.com/u2takey/ffmpeg-go"
+	"log"
 	"net/http"
 	"os"
 	"path"
+	"path/filepath"
 	"strconv"
 )
 
@@ -20,13 +22,13 @@ func PublishAction(c *gin.Context) {
 	file := form.File["data"]
 	title := form.Value["title"][0]
 	var userinfo model.User
-	userid, _ := c.Get("username")
+	userid, _ := c.Get("userid")
 	db.Db.Table("users").Where("id = ?", userid).First(&userinfo)
 	var data = make([]byte, file[0].Size)
 	contenType := http.DetectContentType(data)
 	if contenType != "video/mp4" {
 		message := "invalid content type"
-		c.JSON(http.StatusResetContent, model.Response{
+		c.JSON(http.StatusResetContent, Response{
 			StatusCode: 1,
 			StatusMsg:  message,
 		})
@@ -35,7 +37,7 @@ func PublishAction(c *gin.Context) {
 	openFile, err := file[0].Open()
 	if err != nil {
 		message := "Open file failed"
-		c.JSON(http.StatusUnauthorized, &model.Response{
+		c.JSON(http.StatusUnauthorized, Response{
 			StatusCode: 1,
 			StatusMsg:  message,
 		})
@@ -44,7 +46,7 @@ func PublishAction(c *gin.Context) {
 	readSize, _ := openFile.Read(data)
 	if readSize != int(file[0].Size) {
 		message := "Size not match"
-		c.JSON(http.StatusUnauthorized, model.Response{
+		c.JSON(http.StatusUnauthorized, Response{
 			StatusCode: 1,
 			StatusMsg:  message,
 		})
@@ -56,7 +58,7 @@ func PublishAction(c *gin.Context) {
 	err = os.MkdirAll(dir, os.FileMode(0755))
 	if err != nil {
 		message := "Failed to create directory"
-		c.JSON(http.StatusInternalServerError, model.Response{
+		c.JSON(http.StatusInternalServerError, Response{
 			StatusCode: 1,
 			StatusMsg:  message,
 		})
@@ -66,24 +68,16 @@ func PublishAction(c *gin.Context) {
 	err = os.WriteFile(filePath, data, os.FileMode(0755))
 	if err != nil {
 		message := "Failed to save file"
-		c.JSON(http.StatusUnauthorized, model.Response{
+		c.JSON(http.StatusUnauthorized, Response{
 			StatusCode: 1,
 			StatusMsg:  message,
 		})
 		return
 	}
-	coverurl := GetCover(filePath)
-	playurl := VideoConvert(filePath)
+	host := c.Request.Host
+	coverurl, _ := GetCover(filePath, host)
+	playurl, _ := VideoConvert(filePath, host)
 	var Video model.Video
-	err = db.Db.AutoMigrate(&model.Video{})
-	if err != nil {
-		message := "Failed to create a table"
-		c.JSON(http.StatusInternalServerError, model.Response{
-			StatusCode: 1,
-			StatusMsg:  message,
-		})
-		return
-	}
 	Video = model.Video{
 		PlayURL:  playurl,
 		Title:    title,
@@ -91,78 +85,76 @@ func PublishAction(c *gin.Context) {
 		CoverURL: coverurl,
 	}
 	db.Db.Save(&Video)
-	result2 := db.Db.Table("users").Where("ID = ?", userid).Update("work_count", userinfo.WorkCount+1)
-	if result2.Error != nil {
-		message := "Failed to Update"
-		c.JSON(http.StatusInternalServerError, model.Response{
-			StatusCode: 1,
-			StatusMsg:  message,
-		})
-		return
-	}
 	message := "Success"
-	c.JSON(http.StatusOK, model.Response{
+	c.JSON(http.StatusOK, Response{
 		StatusCode: 0,
 		StatusMsg:  message,
 	})
 }
-func GetCover(filePath string) (coverurl string) {
+func GetCover(filePath string, host string) (coverurl string, err error) {
+	const (
+		imageFormat = "png"
+		frameNumber = 1
+	)
 	inputFile := filePath
 	imagename := ksuid.New().String()
-	outfile := path.Join("cover", imagename+".png")
-	dir := path.Dir(outfile)
-	err := os.MkdirAll(dir, os.FileMode(0755))
+	outfile := filepath.Join("/douyin/publish/list/", imagename+"."+imageFormat)
+	dir := filepath.Dir(outfile)
+	err = os.MkdirAll(dir, os.FileMode(0755))
 	if err != nil {
-		return ""
+		log.Println("Failed to create directory:", err)
+		return
 	}
 	buf := bytes.NewBuffer(nil)
-	err = ffmpeg_go.Input(inputFile).Filter("select", ffmpeg_go.Args{fmt.Sprintf("gte(n,%d)", 1)}).
-		Output(outfile, ffmpeg_go.KwArgs{"vframes": 1, "format": "image2", "vcodec": "png"}).
+	err = ffmpeg_go.Input(inputFile).Filter("select", ffmpeg_go.Args{fmt.Sprintf("gte(n,%d)", frameNumber)}).
+		Output(outfile, ffmpeg_go.KwArgs{"vframes": frameNumber, "format": "image2", "vcodec": "png"}).
 		WithOutput(buf, os.Stdout).Run()
 	if err != nil {
-		fmt.Println("Failed to capture image: ", err)
-		return ""
+		log.Println("Failed to capture image: ", err)
+		return
 	}
-	host, _ := os.Hostname()
-	coverpath := string(host) + "/douyin/publish/list/" + outfile
+	coverpath := filepath.Join(host, outfile)
 	img, err := imaging.Decode(buf)
 	if err != nil {
-		fmt.Println("Failed to capture image: ", err)
-		return ""
+		log.Println("Failed to decode image: ", err)
+		return
 	}
 	err = imaging.Save(img, coverpath)
 	if err != nil {
-		fmt.Println("Failed to capture image: ", err)
-		return ""
+		log.Println("Failed to save image: ", err)
+		return
 	}
-	return coverpath
+	return coverpath, nil
 }
-func VideoConvert(filepath string) (palyurl string) {
-	inputFile := filepath
+func VideoConvert(filePath string, host string) (palyurl string, err error) {
+	const videoFormat = "mp4"
+	inputFile := filePath
 	filename := ksuid.New().String()
-	outfile := path.Join("out", filename+".mp4")
-	dir := path.Dir(outfile)
-	_ = os.MkdirAll(dir, os.FileMode(0755))
-	err := ffmpeg_go.Input(inputFile).Output(outfile, ffmpeg_go.KwArgs{
+	outfile := filepath.Join("/douyin/publish/action/", filename+"."+videoFormat)
+	dir := filepath.Dir(outfile)
+	err = os.MkdirAll(dir, os.FileMode(0755))
+	if err != nil {
+		log.Println("Failed to create directory:", err)
+		return
+	}
+	err = ffmpeg_go.Input(inputFile).Output(outfile, ffmpeg_go.KwArgs{
 		"profile:v": "main",
 		"movflags":  "+faststart",
 		"crf":       26,
 	}).OverWriteOutput().Run()
 	if err != nil {
-		fmt.Println("Transcoding failed", err)
-		return ""
+		log.Println("Transcoding failed", err)
+		return
 	}
-	host, _ := os.Hostname()
-	videourl := string(host) + "/douyin/publish/action/" + outfile
-	return videourl
+	videourl := filepath.Join(host, outfile)
+	return videourl, nil
 }
 func PublishList(c *gin.Context) {
-	_, tokenbool := c.GetQuery("token")
 	userId, userIdbool := c.GetQuery("user_id")
 	userid, _ := strconv.ParseInt(userId, 10, 64)
-	if !tokenbool || !userIdbool {
+	if !userIdbool {
 		message := "no passed data"
-		c.JSON(http.StatusInternalServerError, model.Response{
+		c.JSON(http.StatusInternalServerError, Response{
 			StatusCode: 1,
 			StatusMsg:  message,
 		})
@@ -172,7 +164,7 @@ func PublishList(c *gin.Context) {
 	err := db.Db.Table("users").Where("id = ?", userid).First(&userinfo)
 	if err.Error != nil {
 		message := "no pass data"
-		c.JSON(http.StatusInternalServerError, model.Response{
+		c.JSON(http.StatusInternalServerError, Response{
 			StatusCode: 1,
 			StatusMsg:  message,
 		})
@@ -183,16 +175,16 @@ func PublishList(c *gin.Context) {
 	err = db.Db.Table("videos").Limit(count).Where("user_id = ?", userinfo.ID).Find(&video)
 	if err.Error != nil {
 		message := "Failed to search Video list"
-		c.JSON(http.StatusInternalServerError, model.Response{
+		c.JSON(http.StatusInternalServerError, Response{
 			StatusCode: 1,
 			StatusMsg:  message,
 		})
 		return
 	}
-	var videos []*model.VideoRsp
+	var videos []*VideoRsp
 	for _, v := range video {
 		User := GetUser(v.UserId)
-		videos = append(videos, &model.VideoRsp{
+		videos = append(videos, &VideoRsp{
 			Author:        User,
 			CommentCount:  v.CommentCount,
 			CoverURL:      v.CoverURL,
@@ -204,11 +196,19 @@ func PublishList(c *gin.Context) {
 		})
 	}
 	Smessage := "Success"
-	c.JSON(http.StatusOK, model.ListRsp{
-		Response: model.Response{
-			StatusCode: 0,
-			StatusMsg:  Smessage,
-		},
-		VideoList: videos,
+	c.JSON(http.StatusOK, ListRsp{
+		StatusCode: 0,
+		StatusMsg:  Smessage,
+		VideoList:  videos,
 	})
+}
+
+type Response struct {
+	StatusCode int64  `json:"status_code"` // 状态码，0-成功，其他值-失败
+	StatusMsg  string `json:"status_msg"`  // 返回状态描述
+}
+type ListRsp struct {
+	StatusCode int64       `json:"status_code"` // 状态码，0-成功，其他值-失败
+	StatusMsg  string      `json:"status_msg"`  // 返回状态描述
+	VideoList  []*VideoRsp `json:"video_list"`  // 用户发布的视频列表
 }
